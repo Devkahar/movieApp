ed577379c9224279f88c89201fe5eec2570f4781
Dev Kahar <devkahar99@gmail.com>
2022-06-20 11:55:28 +0530

    Added Movie Tab Card Widget

---
+++ lib/presentation/journeys/home/movie_tab/movie_tab_card_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/common/constants/size_constants.dart';
+import 'package:movie_app/common/extensions/size_extension.dart';
+import 'package:movie_app/core/api_constants.dart';
+
+class MovieTabCardWidget extends StatelessWidget {
+  final String title;
+  final String posterPath;
+
+  const MovieTabCardWidget(
+      {Key? key, required this.title, required this.posterPath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){},
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(Sizes.dimen_16.w.toDouble()),
+              child: Image.network(
+                '${ApiConstanst.BASE_IMG_URL}$posterPath',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
